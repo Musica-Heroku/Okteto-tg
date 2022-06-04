8aa81d2bdde24abd8134d794a244455b1cb91ab4
jitendra.makwana <jitendra.makwana@JITENDRA-263RK>
2022-06-04 10:51:18 +0530

    Changes

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from pyrogram import enums
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait
 
-teletips = Client(
+bot = Client(
     "LOCAL",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
 )
 
-@teletips.on_message(filters.command("start"))
+@bot.on_message(filters.command("start"))
 async def start(client: Client, message: Message):
     await client.send_message(message.chat.id, "Hello there")
+
+print("PingAll is alive!")  
+bot.run()
