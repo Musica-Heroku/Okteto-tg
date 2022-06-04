824aba2719dbf029f19dab6995b3f331211ac4b8
jitendra.makwana <jitendra.makwana@JITENDRA-263RK>
2022-06-04 10:35:03 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import os
+import asyncio
+from pyrogram import enums
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import FloodWait
+
+teletips = Client(
+    "LOCAL",
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    bot_token = os.environ["BOT_TOKEN"]
+)
+
+@teletips.on_message(filters.command("start"))
+async def start(client: Client, message: Message):
+    await client.send_message(message.chat.id, "Hello there")
